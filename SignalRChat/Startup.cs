0414d788d0d43f8a55afad85c8f9d35fa5879d48
Ivan Ivanov <slayer_word_11@outlook.com>
2015-08-21 19:22:10 +0300

    update ChatHub class

--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
+
 [assembly: OwinStartup(typeof(SignalRChat.Startup))]
 namespace SignalRChat
 {
@@ -7,7 +8,6 @@ namespace SignalRChat
     {
         public void Configuration(IAppBuilder app)
         {
-            // Any connection or hub wire up and configuration should go here
             app.MapSignalR();
         }
     }
